1d3bda8be77031c1456964dda3bdcb7aa084904f
Gabriel Pereyra R <pereyrarg11@gmail.com>
2023-04-08 21:13:36 -0600

    chore: use-case and repository to request data from remote

--- app/src/main/java/com/pereyrarg11/cocktail/categories/alcoholic/ui/AlcoholicScreen.kt
+++ app/src/main/java/com/pereyrarg11/cocktail/categories/alcoholic/ui/AlcoholicScreen.kt
@@ -3,8 +3,15 @@ package com.pereyrarg11.cocktail.categories.alcoholic.ui
 import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.produceState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.pereyrarg11.cocktail.categories.ui.CategoriesUiState
+import com.pereyrarg11.cocktail.categories.ui.CategoriesUiState.*
 import com.pereyrarg11.cocktail.categories.ui.CocktailColumn
 
 @Composable
@@ -12,12 +19,33 @@ fun AlcoholicScreen(
     modifier: Modifier = Modifier,
     viewModel: AlcoholicViewModel = viewModel(),
 ) {
-    Column(modifier = modifier) {
-        CocktailColumn(
-            rowList = viewModel.cocktailList,
-            onClickItem = { model ->
-                Log.i("AlcoholicScreen", "you have clicked on ${model.name}")
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+    val uiState by produceState<CategoriesUiState>(
+        initialValue = Loading,
+        key1 = lifecycle,
+        key2 = viewModel
+    ) {
+        lifecycle.repeatOnLifecycle(state = Lifecycle.State.STARTED) {
+            viewModel.uiState.collect { value = it }
+        }
+    }
+
+    when (uiState) {
+        is Error -> {
+            // TODO: define a composable for this case
+        }
+        Loading -> {
+            // TODO: define a composable for this case
+        }
+        is Success -> {
+            Column(modifier = modifier) {
+                CocktailColumn(
+                    rowList = (uiState as Success).cocktailList,
+                    onClickItem = { model ->
+                        Log.i("AlcoholicScreen", "you have clicked on ${model.name}")
+                    }
+                )
             }
-        )
+        }
     }
 }
--- app/src/main/java/com/pereyrarg11/cocktail/categories/alcoholic/ui/AlcoholicViewModel.kt
+++ app/src/main/java/com/pereyrarg11/cocktail/categories/alcoholic/ui/AlcoholicViewModel.kt
@@ -1,27 +1,25 @@
 package com.pereyrarg11.cocktail.categories.alcoholic.ui
 
 import androidx.lifecycle.ViewModel
-import com.pereyrarg11.cocktail.categories.ui.model.CocktailModel
+import androidx.lifecycle.viewModelScope
+import com.pereyrarg11.cocktail.categories.alcoholic.domain.GetAlcoholicCocktailListUseCase
+import com.pereyrarg11.cocktail.categories.ui.CategoriesUiState
+import com.pereyrarg11.cocktail.categories.ui.CategoriesUiState.*
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
-class AlcoholicViewModel @Inject constructor() : ViewModel() {
+@HiltViewModel
+class AlcoholicViewModel @Inject constructor(
+    getAlcoholicCocktailListUseCase: GetAlcoholicCocktailListUseCase,
+) : ViewModel() {
 
-    val cocktailList: List<CocktailModel>
-        get() = listOf(
-            CocktailModel(
-                name = "Applejack",
-                thumbnailUrl = "https://www.thecocktaildb.com/images/media/drink/sutyqp1479209062.jpg",
-                id = "16311"
-            ),
-            CocktailModel(
-                name = "Margarita",
-                thumbnailUrl = "https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg",
-                id = "11007"
-            ),
-            CocktailModel(
-                name = "Mojito",
-                thumbnailUrl = "https://www.thecocktaildb.com/images/media/drink/metwgh1606770327.jpg",
-                id = "11000"
-            ),
+    val uiState: StateFlow<CategoriesUiState> = getAlcoholicCocktailListUseCase()
+        .map(::Success)
+        .catch { Error(it) }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000),
+            Loading
         )
 }
