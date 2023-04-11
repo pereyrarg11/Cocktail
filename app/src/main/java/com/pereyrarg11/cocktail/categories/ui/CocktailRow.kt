f4743eba17bdedd519adf8ca327a66f49b6d6fbd
Gabriel Pereyra R <pereyrarg11@gmail.com>
2023-04-10 21:29:48 -0600

    chore: load images with Glide

--- app/src/main/java/com/pereyrarg11/cocktail/categories/ui/CocktailRow.kt
+++ app/src/main/java/com/pereyrarg11/cocktail/categories/ui/CocktailRow.kt
@@ -1,9 +1,7 @@
 package com.pereyrarg11.cocktail.categories.ui
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,10 +9,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.pereyrarg11.cocktail.R
 import com.pereyrarg11.cocktail.categories.ui.model.CocktailModel
 import com.pereyrarg11.cocktail.ui.theme.CocktailTheme
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun CocktailRow(
     model: CocktailModel,
@@ -32,10 +34,15 @@ fun CocktailRow(
     ) {
         Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(dimensionResource(id = R.dimen.margin_md)),
+                .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
         ) {
+            GlideImage(
+                model = "${model.thumbnailUrl}/preview",
+                contentDescription = null,
+                modifier = Modifier.size(100.dp),
+            )
+            Spacer(modifier = Modifier.size(dimensionResource(id = R.dimen.margin_md)))
             Text(text = model.name)
         }
     }
