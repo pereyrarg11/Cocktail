f1a452b54704431065c16cf975a42d77eb93bdb5
Gabriel Pereyra R <pereyrarg11@gmail.com>
2023-04-17 17:02:50 -0600

    chore: basic layout of drink details

--- app/src/main/java/com/pereyrarg11/cocktail/detail/ui/DrinkDetailScreen.kt
+++ app/src/main/java/com/pereyrarg11/cocktail/detail/ui/DrinkDetailScreen.kt
@@ -1,8 +1,8 @@
 package com.pereyrarg11.cocktail.detail.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavHostController
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
@@ -28,11 +29,22 @@ fun DrinkDetailScreen(
     Scaffold(
         topBar = { DrinkDetailAppBar(navController) },
     ) {
-        Column(modifier = modifier) {
+        Column(
+            modifier = modifier
+                .padding(bottom = dimensionResource(id = R.dimen.size_md))
+                .verticalScroll(rememberScrollState())
+        ) {
             DrinkDetailImage(
                 imageUrl = drinkDetail.imageUrl,
                 imageCopyright = drinkDetail.imageCopyright
             )
+            DrinkDetailName(title = drinkDetail.name)
+            DrinkDetailSubtitle(label = stringResource(R.string.title_glass_type))
+            DrinkDetailContent(label = drinkDetail.glass)
+            DrinkDetailSubtitle(label = stringResource(R.string.title_ingredients))
+            DrinkDetailContent(label = drinkDetail.ingredients.joinToString(separator = "\n"))
+            DrinkDetailSubtitle(label = stringResource(R.string.title_preparation))
+            DrinkDetailContent(label = drinkDetail.instructions)
         }
     }
 }
@@ -94,6 +106,53 @@ fun DrinkDetailImage(
             )
         }
     }
+}
+
+@Composable
+fun DrinkDetailName(
+    title: String,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = title,
+        style = MaterialTheme.typography.headlineLarge,
+        modifier = modifier
+            .padding(horizontal = dimensionResource(id = R.dimen.size_md)),
+    )
+}
+
+@Composable
+fun DrinkDetailSubtitle(
+    label: String,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier.padding(horizontal = dimensionResource(id = R.dimen.size_md))
+    ) {
+        Spacer(modifier = Modifier.size(dimensionResource(id = R.dimen.size_md)))
+        Text(
+            text = label,
+            style = MaterialTheme.typography.headlineSmall,
+        )
+    }
+}
+
+@Composable
+fun DrinkDetailContent(
+    label: String,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = label,
+        style = MaterialTheme.typography.bodyMedium,
+        modifier = modifier.padding(horizontal = dimensionResource(id = R.dimen.size_md)),
+    )
+}
+
+@Preview
+@Composable
+fun DrinkDetailTitlePreview() {
+    DrinkDetailName(getDrinkDetailDisplayable().name)
 }
 
 fun getDrinkDetailDisplayable(): DrinkDetailDisplayable {
